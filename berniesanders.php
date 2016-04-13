aaa89a6a1571fb67afe36aac13bbb51484f0f2c1
pwhitter1 <pwhitter1@gwu.edu>
2016-04-13 08:27:44 -0400

    Removed links to State page and addded links to Home page from candidate pages.

--- berniesanders.php
+++ berniesanders.php
@@ -39,14 +39,11 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">Home</a>
+                <a class="navbar-brand" href="index.html">Home</a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
-                    <li>
-                        <a href="state.html">State</a>
-                    </li>
                     <li>
                         <a href="year.html">Year</a>
                     </li>
